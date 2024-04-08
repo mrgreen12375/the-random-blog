b4572f06cf98d6a359c19fa45ed9c2fbef089430
Steven Green <ste2144105@gmail.com>
2024-04-08 12:06:40 -0700

    copied models folder from local files

---
+++ models/blog.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {
+
+}
+
+Blog.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+
+    },
+    create_on_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+
+    },
+
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+
+  }
+);
+
+module.exports = Blog;
